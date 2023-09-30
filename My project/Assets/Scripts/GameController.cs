1d312148eab99a4b6a6180165e6068582e1a94db
Igor <52105996+IgorPalii@users.noreply.github.com>
2023-09-30 12:34:52 +0300

    Lesson4
    Lesson4

--- My project/Assets/Scripts/GameController.cs
+++ My project/Assets/Scripts/GameController.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SearchService;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
@@ -22,5 +24,29 @@ public class GameController : MonoBehaviour
     private void Update()
     {
         scoreText.text = "Score: " + score.ToString();
+    }
+
+    private void BackToMenu()
+    {
+        Time.timeScale = 1;
+        SceneManager.LoadScene("Menu");
+    }
+
+    public void SwitchPause()
+    {
+        pausePanel.SetActive(!pausePanel.activeSelf);
+        gamePanel.SetActive(!gamePanel.activeSelf);
+
+        switch(Time.timeScale)
+        {
+            case 1:
+                Time.timeScale = 0;
+                break;
+            case 0:
+                Time.timeScale = 1;
+                break;
+            default:
+                break;
+        }
     }
 }
